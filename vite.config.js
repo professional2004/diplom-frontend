0520998ca6fb494c3907652a1500087bb91aa2b3
Alesia <Alesia.Loika@gmail.com>
2026-01-22 09:46:56 +0300

    ThreeJS и TresJS подключила библиотеки (без настроек)

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { templateCompilerOptions } from '@tresjs/core'
 import fs from 'fs'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue({
+      template: templateCompilerOptions
+    })
   ],
   server: {
     port: 5173,
