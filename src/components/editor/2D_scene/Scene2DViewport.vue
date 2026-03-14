bae7fb6bf0c873557c6ee738cb33166bd3d8b3ac
Alesia <Alesia.Loika@gmail.com>
2026-03-14 12:49:46 +0300

    переименован containerref в canvasref

--- src/components/editor/2D_scene/Scene2DViewport.vue
+++ src/components/editor/2D_scene/Scene2DViewport.vue
@@ -3,18 +3,18 @@ import { ref, onMounted } from 'vue'
 import { useEditorStore } from '@/stores/editorStore'
 import ToolbarPanel2D from '@/components/editor/2D_scene/ToolbarPanel2D.vue'
 
-const containerRef = ref(null)
+const canvasRef = ref(null)
 const editorStore = useEditorStore()
 
 onMounted(() => {
-  if (!containerRef.value) return
-  editorStore.init2D(containerRef.value)
+  if (!canvasRef.value) return
+  editorStore.init2D(canvasRef.value)
 })
 </script>
 
 <template>
   <div class="scene2d-viewport">
-    <div ref="containerRef" class="viewport"></div>
+    <div ref="canvasRef" class="viewport"></div>
     <div class="ui-layer">
       <ToolbarPanel2D />
     </div>
